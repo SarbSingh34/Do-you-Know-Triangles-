ae3e38033f2e9a085e63c4a9ccd4456277c30943
Sarbkalyan Singh <sarbbsandhu555@gmail.com>
2021-09-30 23:41:03 +0530

    done

--- app.js
+++ app.js
@@ -39,3 +39,70 @@ const  button  =  document.querySelector("#tribtn");
       return player;
       
   }
+
+
+//  sNew  
+
+
+const new1 = document.querySelector(".formmain");
+
+const newbtn = document.querySelector("#submitbtn");
+
+const correctanswers = ["90","right angled"];
+/* 3rd line is used for comparing the correct answers at last. */
+
+
+ newbtn.addEventListener("click", calculatescore );
+
+
+  function calculatescore()
+{
+   let score = 0;    /* 1st one is used as initializer  */
+   let moveinsider = 0;   /* 2nd is used as go through or passer in looop  */
+   
+   const formresults = new FormData(new1);  
+   /* This is used to get access form API  and value is stored in new var  */
+    for(let value of formresults.values())
+    /*  for & of is used to get input-value pair data 
+      .entries is  used as  special method
+    */
+    {
+    
+       if( value === correctanswers[moveinsider])
+       {
+              
+            score = score + 1;
+       
+       }
+       
+       moveinsider = moveinsider + 1 ;  
+    
+    }
+     console.log(score);
+}
+
+
+
+
+const  new1 = document.querySelector("#firstmove");
+
+const  new2 = document.querySelector("#secondmove");
+
+calculatebtn = document.querySelector("#cal-btn");
+
+calculatebtn.addEventListener("click", calculatesum);
+
+   function calculatesum()
+{
+    const firstpart =  calculatesumofsquare  (Number (new1.value), Number (new2.value));
+       const calculatefinalsquare = Math.sqrt(firstpart);
+        console.log(calculatefinalsquare);
+      
+};
+
+     function calculatesumofsquare(a,b)
+   {
+         const calculatenumber = a*a + b*b ;
+         return calculatenumber ;
+   }
+
